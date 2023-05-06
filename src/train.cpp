e2740945161b771bb61e381cccdaa33adebddf7b
Кандрушин Максим 22-IST-1 <125025831+heiby-baby@users.noreply.github.com>
2023-05-06 19:21:47 +0300

    Update train.cpp

--- src/train.cpp
+++ src/train.cpp
@@ -1,2 +1,47 @@
 // Copyright 2021 NNTU-CS
 #include "train.h"
+
+void Train::addCage(bool light) {
+  Cage* cur = new Cage;
+  cur->light = light;
+  if (first == nullptr) {
+    first = cur;
+    first->prev = first;
+    first->next = first;
+  } else {
+    cur->next = first;
+    cur->prev = first->prev;
+    first->prev->next = cur;
+    first->prev = cur;
+  }
+}
+
+int Train::getLength() {
+  int  Dynamic_counter = 1;
+  Cage* cur = first;
+  if (cur == nullptr) return 0;
+  cur->light = true;
+  countOp++;
+  cur = cur->next;
+  while (cur) {
+    if (cur->light == false) {
+      Dynamic_counter++;
+      countOp++;
+      cur = cur->next;
+    } else {
+      cur->light = false;
+      for (int i = 0; i < Dynamic_counter; i++) {
+        countOp++;
+        cur = cur->prev;
+      }
+			if (cur->light == false) {
+        return Dynamic_counter;
+      } else {
+        Dynamic_counter = 1;
+        countOp++;
+        cur = cur->next;
+      }
+    }
+  }
+  return 0;
+}
